ec4ecf90a6ccc21377c33217062de97a913167b0
macgors <244991@student.pwr.edu>
2019-05-23 15:57:56 +0200

    Working on stands

--- SpaceWeekApp/app/src/main/java/com/example/spaceweekapp/fragments/StandsElements/StandAdapter.kt
+++ SpaceWeekApp/app/src/main/java/com/example/spaceweekapp/fragments/StandsElements/StandAdapter.kt
@@ -18,14 +18,14 @@ import com.example.spaceweekapp.R
 import java.security.AccessController.getContext
 
 
-class StandAdapter (val context: Context, private val list: List<HashMap<String, Any>>,val manager: FragmentManager) : RecyclerView.Adapter<MovieViewHolder>() {
+class StandAdapter (val context: Context, private val list: List<HashMap<String, Any>>,val manager: FragmentManager) : RecyclerView.Adapter<StandViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StandViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-        return MovieViewHolder(inflater, parent)
+        return StandViewHolder(inflater, parent)
     }
 
-    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: StandViewHolder, position: Int) {
         var stand: HashMap<String, Any> = list[position]
         holder.bind(stand, position, manager, context)
     }
@@ -53,15 +53,16 @@ class StandViewHolder(inflater: LayoutInflater, parent: ViewGroup) :
         organisation = itemView.findViewById(com.example.spaceweekapp.R.id.stand_organisation)
         photo=itemView.findViewById(com.example.spaceweekapp.R.id.stand_photo)
 
+
     }
 
     @SuppressLint("SetTextI18n")
     fun bind(stand: HashMap<String, Any>, position: Int, manager: FragmentManager, context : Context)  {
-        title?.text="title"
-        description?.text="desc"
-        place?.text="place"
-        long_place?.text="long_place"
-        organisation?.text="org"
+        title?.text=stand.getOrDefault("title", " ") as String
+        description?.text=stand.getOrDefault("description", " ") as String
+        place?.text=stand.getOrDefault("place", " ") as String
+        long_place?.text=stand.getOrDefault("long_place", " ") as String
+        organisation?.text=stand.getOrDefault("organisation", " ") as String
         photo?.setImageResource(R.drawable.stands)
 
     }
