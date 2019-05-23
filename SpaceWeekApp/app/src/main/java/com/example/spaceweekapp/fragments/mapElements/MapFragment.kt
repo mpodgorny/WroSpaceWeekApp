7f824b77a3317a9c596a73d536ef466353d8ec02
macgors <244991@student.pwr.edu>
2019-05-23 23:39:07 +0200

    Map became zoomable
    Map is now zoomable, also added icon - matter of the highest importance.

--- SpaceWeekApp/app/src/main/java/com/example/spaceweekapp/fragments/mapElements/MapFragment.kt
+++ SpaceWeekApp/app/src/main/java/com/example/spaceweekapp/fragments/mapElements/MapFragment.kt
@@ -3,6 +3,12 @@ package com.example.spaceweekapp.fragments.mapElements
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.*
+import android.provider.SyncStateContract.Helpers.update
+import com.example.spaceweekapp.R
+import kotlinx.android.synthetic.main.map.view.*
+import uk.co.senab.photoview.PhotoViewAttacher
+
+
 
 
 class MapFragment : Fragment() {
@@ -16,8 +22,12 @@ class MapFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater!!.inflate(com.example.spaceweekapp.R.layout.map, container, false)
 
+
+        val view = inflater!!.inflate(com.example.spaceweekapp.R.layout.map, container, false)
+       val pAttacher: PhotoViewAttacher
+        pAttacher = PhotoViewAttacher(view.findViewById(R.id.mapIamge))
+        pAttacher.update()
 
         return view
     }
