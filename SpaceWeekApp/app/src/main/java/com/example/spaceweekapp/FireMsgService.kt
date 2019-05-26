eb265887b8d9cd80296d8f20557dac62b25a6f76
s7e0n5g1a <44317166+s7e0n5g1a@users.noreply.github.com>
2019-05-26 23:53:17 +0200

    notification

--- SpaceWeekApp/app/src/main/java/com/example/spaceweekapp/FireMsgService.kt
+++ SpaceWeekApp/app/src/main/java/com/example/spaceweekapp/FireMsgService.kt
@@ -1,31 +1,64 @@
 package com.example.spaceweekapp
-
+import android.app.Notification
 import android.app.NotificationManager
 import android.content.Context
-import android.media.RingtoneManager
-import android.support.v4.app.NotificationCompat
-import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
-import android.support.v4.content.ContextCompat.getSystemService
-import android.R
-import android.app.Notification
 import android.app.NotificationChannel
+import android.app.PendingIntent
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.graphics.Color
 import android.os.Build
-import android.support.v4.app.NotificationManagerCompat
-import java.security.AccessController.getContext
-
+import android.widget.RemoteViews
 
 class FireMsgService : FirebaseMessagingService() {
 
+    lateinit var builder : Notification.Builder
+    lateinit var notificationManager : NotificationManager
+    lateinit var notificationChannel : NotificationChannel
+
+    private val channelId = "com.example.spaceweekapp"
+    private val description = "Test notification"
 
    override fun onMessageReceived(remoteMessage: RemoteMessage) {
         super.onMessageReceived(remoteMessage)
-       println("------------------------------------")
-            println("Firebase MSG"+ "" + remoteMessage.notification!!.body)
+     //  println("------------------------------------")
+       //     println("Firebase MSG"+ "" + remoteMessage.notification!!.body)
+      // val napis = remoteMessage.notification!!.body
 
-       //TODO: make msg into notification
+       notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+       val intent = Intent(this,MainActivity::class.java)
+       val pendingIntent = PendingIntent.getActivity(this,0,intent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+       val contentView = RemoteViews(packageName, R.layout.notification_layout)
+       contentView.setTextViewText(R.id.tv_title,"SpaceWeek")
+       contentView.setTextViewText(R.id.tv_content,remoteMessage.notification!!.body)
+
+       if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+           notificationChannel = NotificationChannel(channelId,description,NotificationManager.IMPORTANCE_HIGH)
+           notificationChannel.enableLights(true)
+           notificationChannel.lightColor = Color.GREEN
+           notificationChannel.enableVibration(false)
+           notificationManager.createNotificationChannel(notificationChannel)
+
+           builder = Notification.Builder(this,channelId)
+               .setContent(contentView)
+               .setSmallIcon(R.drawable.logo_small_purple)
+               .setLargeIcon(BitmapFactory.decodeResource(this.resources, R.drawable.logo_small_purple))
+               .setContentIntent(pendingIntent)
+       }else{
+
+           builder = Notification.Builder(this)
+               .setContent(contentView)
+               .setSmallIcon(R.drawable.logo_small_purple)
+               .setLargeIcon(BitmapFactory.decodeResource(this.resources, R.drawable.logo_small_purple))
+               .setContentIntent(pendingIntent)
+       }
+       notificationManager.notify(1234,builder.build())
 
    }
 }
+
 
